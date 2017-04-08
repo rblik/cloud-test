3b1a4bd3c89ce9a428655f9d49317060671940f5
Blik <rblick@bk.ru>
2017-04-08 23:53:57 +0300

    3-rd

--- src/main/java/isr/ek0/CloudTestApplication.java
+++ src/main/java/isr/ek0/CloudTestApplication.java
@@ -2,14 +2,18 @@ package isr.ek0;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -30,6 +34,18 @@ public class CloudTestApplication {
             asList("Me", "Myself", "AND I").forEach(s -> rr.save(new Reservation(s)));
             rr.findAll().forEach(System.out::println);
         };
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
     }
 }
 
